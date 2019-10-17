1ec88e9e22dfb49ecede7dda02f54b458fbbc98d
jacquigiese <jacqui.giese@gmail.com>
2019-10-16 17:05:50 -0700

    added DefaultIncident struct with method implementation

--- incident_viewer_demo/incidents/incident.go
+++ incident_viewer_demo/incidents/incident.go
@@ -1,4 +1,5 @@
 package time
+
 import "time"
 
 type Incident interface {
@@ -6,4 +7,34 @@ type Incident interface {
 	getBadPeriod() (time.Time, time.Time)
 	getSeverity() float64
 	getTestsAffected() int
+	getSeverityUnits() string
+}
+
+type DefaultIncident struct {
+	goodStartTime time.Time
+	goodEndTime   time.Time
+	badStartTime  time.Time
+	badEndTime    time.Time
+	severity      float64
+	testsAffected int
+}
+
+func (i DefaultIncident) getGoodPeriod() (time.Time, time.Time) {
+	return i.goodStartTime, i.goodEndTime
+}
+
+func (i DefaultIncident) getBadPeriod() (time.Time, time.Time) {
+	return i.badStartTime, i.badEndTime
+}
+
+func (i DefaultIncident) getSeverity() float64 {
+	return i.severity
+}
+
+func (i DefaultIncident) getTestsAffected() int {
+	return i.testsAffected
+}
+
+func (i DefaultIncident) getSeverityUnits() string {
+	return "percent"
 }
