7fba3828204c32d116681e3cc6759b308be2ae1b
jacquigiese <jacqui.giese@gmail.com>
2019-10-18 11:58:33 -0700

    fix compile errors for getGoodPeriod test

--- incident_viewer_demo/incident/incident_test.go
+++ incident_viewer_demo/incident/incident_test.go
@@ -1,36 +1,43 @@
 package incident
 
 import (
+	"reflect"
 	"testing"
 	"time"
-	"github.com/m-lab/clinic2019/incident_viewer_demo/incident"
 )
+
+type timeAr struct {
+	start time.Time
+	end   time.Time
+}
 
 func Test_getGoodPeriod(t *testing.T) {
 	type args struct {
 	}
 	tests := []struct {
-		name string
+		name  string
 		input DefaultIncident
-		want time.Time, time.Time
+		want  timeAr
 	}{
 		{
-			name: "Return two time objects"
+			name: "Return two time objects",
 			input: DefaultIncident{time.Date(2000, 12, 12, 0, 0, 0, 0, time.UTC),
-									time.Date(2001, 12, 12, 0, 0, 0, 0, time.UTC),
-									time.Date(2001, 12, 12, 1, 0, 0, 0, time.UTC),
-									time.Date(2002, 12, 12, 0, 0, 0, 0, time.UTC),
-									0.5,
-									123456
-								}
-			want: time.Date(2000, 12, 12, 0, 0, 0, 0, time.UTC),
-			time.Date(2001, 12, 12, 0, 0, 0, 0, time.UTC),
-		}
+				time.Date(2001, 12, 12, 0, 0, 0, 0, time.UTC),
+				time.Date(2001, 12, 12, 1, 0, 0, 0, time.UTC),
+				time.Date(2002, 12, 12, 0, 0, 0, 0, time.UTC),
+				0.5,
+				123456,
+			},
+			want: timeAr{time.Date(2000, 12, 12, 0, 0, 0, 0, time.UTC),
+				time.Date(2001, 12, 12, 0, 0, 0, 0, time.UTC)},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := getGoodPeriod(tt.input); !reflect.DeepEqual(got, tt.want) {
+			s, e := (&tt.input).getGoodPeriod()
+			got := timeAr{s, e}
+			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("getGoodPeriod() = %v, want %v", got, tt.want)
 			}
 		})
