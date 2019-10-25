e883757245c1ffb89a5aaa89c83183d10b40ad18
clinc1920 <clinc1920@clinc1920s-iMac.local>
2019-10-25 12:42:25 -0700

    everything is ready minus cleaning little things up

--- CsvParser/csvParser.go
+++ CsvParser/csvParser.go
@@ -2,11 +2,13 @@ package CsvParser
 
 import(
 	"io"
+	"fmt"
 	"time"
   	"log"
 	"os"
 	"strconv"
 	"encoding/csv"
+	"strings"
 )
 
 type Incident interface {
@@ -30,12 +32,16 @@ func (i *DefaultIncident) getSeverity() float64 {
 	return i.severity
 }
 
+func (i *DefaultIncident) getTestsAffected() int {
+	return i.testsAffected
+}
+
 func CsvParser(filePath string) [100]DefaultIncident{
 
 	//just assume that you have 100 rows in the csv and then return an array of 1OO incidents
 	var incidentArray [100]DefaultIncident
 
-	const shortForm = "2006-02-24"
+	const shortForm = "2006-01-02"
 
 	f, err := os.Open(filePath)
 
@@ -47,7 +53,7 @@ func CsvParser(filePath string) [100]DefaultIncident{
 
 	reader := csv.NewReader(f);
 
-	reader.Comma = ';'
+	reader.Comma = ','
 
 	//this reads the header and probably increments r
 	rec, err := reader.Read();
@@ -60,6 +66,10 @@ func CsvParser(filePath string) [100]DefaultIncident{
 	
 		rec, err = reader.Read()
 
+		// fmt.Print(rec)
+		// fmt.Print("about to be weird")
+		// fmt.Print(rec[0])
+
 		if err != nil{
 			if err == io.EOF{
 				break
@@ -67,10 +77,18 @@ func CsvParser(filePath string) [100]DefaultIncident{
 			log.Fatal(err)
 		}
 
-		timeStart, _ := time.Parse(shortForm, rec[3])
-		timeEnd, _ := time.Parse(shortForm, rec[4])
-		severity, _ := strconv.ParseFloat(rec[5], 32)
+		timeStartString := strings.Split(rec[3], " ");
+		timeStart, _:= time.Parse(shortForm, timeStartString[1])
+		
+		timeEndString := strings.Split(rec[4], " ")
+		timeEnd, _ := time.Parse(shortForm, timeEndString[1])
+		
+		severityString := strings.Split(rec[5], " ")
+		severity, _ := strconv.ParseFloat(severityString[1], 64)
+		//fmt.Print(severity)
+		//fmt.Print(status)
 		testsAffected, _ := strconv.Atoi(rec[0])
+		//fmt.Print(testsAffected)
 
 		defaultIncident := DefaultIncident{ timeStart, timeEnd, severity, testsAffected}
 
@@ -79,4 +97,10 @@ func CsvParser(filePath string) [100]DefaultIncident{
 	}
 
 	return incidentArray
+}
+
+func main(){
+
+	arrays := CsvParser("newincidents.cv")
+	fmt.Print(arrays[0])
 }
--- CsvParser/csvParser_test.go
+++ CsvParser/csvParser_test.go
@@ -3,27 +3,80 @@ package CsvParser
 import (
 	"testing"
 	"reflect"
+	"time"
 )
 
 
-func Test_CsvParser(t *testing.T){
+func Test_CsvParserEntries(t *testing.T){
 
 	tests := []struct {
 		name string
 		input string
-		want float64
+		want DefaultIncident
 	}{
 		{
 			name: "The first entry in the array",
 			input: "newincidents.csv",
-			want: 0.318335,
+			want: DefaultIncident {time.Date(2013, time.September, 1, 0, 0, 0, 0, time.UTC), 
+				time.Date(2015, time.September, 1, 0, 0, 0, 0, time.UTC), 0.318335, 134},
 		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T){
 			incidents := CsvParser(tt.input)
-			got := incidents[0].getSeverity()
+			got := incidents[0]
+			if !reflect.DeepEqual(got, tt.want){
+				t.Errorf("CsvParser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_CsvParserSize(t *testing.T){
+
+	tests := []struct {
+		name string
+		input string
+		want int
+	}{
+		{
+			name: "The size of the incidents array",
+			input: "newincidents.csv",
+			want: 100,
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T){
+			incidents := CsvParser(tt.input)
+			got := len(incidents)
+			if !reflect.DeepEqual(got, tt.want){
+				t.Errorf("CsvParser() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_CsvParserFourthEntry(t *testing.T){
+
+	tests := []struct {
+		name string
+		input string
+		want DefaultIncident
+	}{
+		{
+			name: "The first entry in the array",
+			input: "newincidents.csv",
+			want: DefaultIncident {time.Date(2015, time.January, 1, 0, 0, 0, 0, time.UTC), 
+				time.Date(2017, time.August, 1, 0, 0, 0, 0, time.UTC), 0.433592, 65},
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T){
+			incidents := CsvParser(tt.input)
+			got := incidents[3]
 			if !reflect.DeepEqual(got, tt.want){
 				t.Errorf("CsvParser() = %v, want %v", got, tt.want)
 			}
