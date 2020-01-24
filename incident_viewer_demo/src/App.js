ddaf710fe7b138e80d73065182435907ded2432f
clinic1920 <clinic1920@clinic1920s-iMac.local>
2020-01-23 20:33:38 -0800

    fixing style

--- incident_viewer_demo/src/App.js
+++ incident_viewer_demo/src/App.js
@@ -102,7 +102,7 @@ class App extends React.Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <button className="showIncident" onClick={this.toggleIncident}>Toggle Incident Viewer</button> 
+          <button className="showIncident" onClick={this.toggleIncident}><i className="fa fa-exclamation"></i>Toggle Incident Viewer</button> 
           <div className="isp-select-row">
             {/* TODO(amy): Also will need to populate
                 dropdown with only incident ISPs (righ now showing ALL). Will also need toggle the boolean
