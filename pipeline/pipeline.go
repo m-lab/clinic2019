98226e4f8ba715914f9b9baca55d78ede81c07e1
jacquigiese <jacqui.giese@gmail.com>
2020-02-11 18:28:16 -0800

    basic pipeline code with no tests

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -1,19 +1,18 @@
-// TODO: figure out how to run signal searcher from here
-// TODO: and then pass that generated csv through csvparser
-// TODO: so that it can make files in google cloud storage
-
 package pipeline
 
 import (
 	"fmt"
 
 	"github.com/m-lab/clinic2019/csvParser"
+	"github.com/m-lab/clinic2019/incident_viewer_demo/incident"
 )
 
-func runPipeline() {
+func runPipeline() [100]incident.DefaultIncident {
 	// Run script that generates CSV
 	// exec.Command("bash", "-c", "go run github.com/m-lab/signal-searcher  | sort -nk1 > incidents.csv").Output()
 	incidents := csvParser.CsvParser("incidents.csv")
 	fmt.Printf("%d", len(incidents))
+	return incidents // eventually want to take this out
+	// TODO: remove the csv file
 
 }
--- pipeline/pipeline_test.go
+++ pipeline/pipeline_test.go
@@ -1,34 +1,50 @@
 package pipeline
 
 import (
+	"reflect"
 	"testing"
+	"time"
+
+	"github.com/m-lab/clinic2019/incident_viewer_demo/incident"
 )
 
 func Test_runPipeline(t *testing.T) {
 
-	// testIncident :=
+	testIncident := new(incident.DefaultIncident)
+	testIncident.Init(time.Date(2017, time.January, 1, 0, 0, 0, 0, time.UTC).AddDate(-1, 0, 0),
+		time.Date(2017, time.January, 1, 0, 0, 0, 0, time.UTC),
+		time.Date(2017, time.January, 1, 0, 0, 0, 0, time.UTC),
+		time.Date(2019, time.April, 1, 0, 0, 0, 0, time.UTC),
+		31.046068,
+		21.27035,
+		0.326146, 4788063)
 
-	// tests := []struct {
-	// 	name string
+	tests := []struct {
+		name string
 
-	// 	// input string
-	// 	want  incident.DefaultIncident
-	// }{
-	// 	{
-	// 		name:  "The first entry in the incidents array",
-	// 		// input: "incidentfile.csv",
-	// 		want:  *testIncident,
-	// 	},
-	// }
+		// input string
+		want incident.DefaultIncident
+	}{
+		{
+			name: "Test that csv and incident arrays are generated",
+			// input: "incidentfile.csv",
+			want: *testIncident,
+		},
+	}
 
-	// for _, tt := range tests {
-	// t.Run(tt.name, func( /*t *testing.T*/ ) {
-	runPipeline()
-	// TODO: do I need to verify that something was written locally?
-	// TODO: should I make a spy for the two functions i'm calling to make sure they're getting called?
-	// if !reflect.DeepEqual(got, tt.want) {
-	// 	t.Errorf("CsvParser() = %v, want %v", got, tt.want)
-	// }
-	// })
-	// }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			incidents := runPipeline()
+			got := incidents[0]
+			// TODO: do I need to verify that something was written locally?
+			// TODO: should I make a spy for the two functions i'm calling to make sure they're getting called?
+			if !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("runPipeline() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func Test_CsvParser_was_called() {
+
 }
