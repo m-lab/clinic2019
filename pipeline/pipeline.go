b9ce949611a5bda19b16806ad14e344ccfa8ca52
jacquigiese <jacqui.giese@gmail.com>
2020-03-05 21:21:47 -0800

    start making incident producer interface

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -5,18 +5,15 @@ import (
 
 	csvParser "github.com/m-lab/clinic2019/CsvParser"
 	"github.com/m-lab/clinic2019/incident_viewer_demo/incident"
-	"github.com/m-lab/signal-searcher/analyzer"
 )
 
 type IncidentProducer interface {
-	findIncidents() []analyzer.Incident
+	FindIncidents()
 }
 
-type incidentArray struct {
-	arr [100]incident.DefaultIncident
-}
+type AnalyzerIncidents struct{}
 
-func (i incidentArray) findIncidents() {
+func (ai *AnalyzerIncidents) findIncidents() {
 	exec.Command("bash", "-c", "go run github.com/m-lab/signal-searcher  | sort -nk1 > incidents.csv").Output()
 }
 
