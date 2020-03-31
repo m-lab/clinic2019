bc9309ab9735786363c62a43d578387864f766bf
Jacqui Giese <jacquigiese@Jacquis-MacBook-Pro.local>
2020-03-31 16:48:57 -0700

    rm csv

--- incident_viewer_demo/incident/incident.go
+++ incident_viewer_demo/incident/incident.go
@@ -1,21 +1,21 @@
 package incident
 
 import (
-	"strconv"
 	"time"
 )
 
 //* All incident types must implement the Incident interface *//
 type Incident interface {
-	GetGoodPeriod() (time.Time, time.Time)
-	GetBadPeriod() (time.Time, time.Time)
-	GetGoodMetric() float64
-	GetBadMetric() float64
-	GetSeverity() float64
-	GetTestsAffected() int
-	GetGoodPeriodInfo() string
-	GetBadPeriodInfo() string
-	GetIncidentInfo() string
+	MakeIncidentData() *IncidentData // put all below methods into this one
+	// GetGoodPeriod() (time.Time, time.Time)
+	// GetBadPeriod() (time.Time, time.Time)
+	// GetGoodMetric() float64
+	// GetBadMetric() float64
+	// GetSeverity() float64
+	// GetTestsAffected() int
+	// GetGoodPeriodInfo() string
+	// GetBadPeriodInfo() string
+	// GetIncidentInfo() string
 }
 
 //* This is the incident format for the JSON file *//
@@ -49,90 +49,90 @@ type DefaultIncident struct {
 //    		CONSTRUCTORS
 //**************************************//
 
-func (i *IncidentData) Init(
-	goodTimeStart time.Time,
-	goodTimeEnd time.Time,
-	badTimeStart time.Time,
-	badTimeEnd time.Time,
-	goodMetric float64,
-	badMetric float64,
-	severity float64,
-	testsAffected int,
-	goodPeriodInfo string,
-	badPeriodInfo string,
-	incidentInfo string) {
+// func (i *IncidentData) Init(
+// 	goodTimeStart time.Time,
+// 	goodTimeEnd time.Time,
+// 	badTimeStart time.Time,
+// 	badTimeEnd time.Time,
+// 	goodMetric float64,
+// 	badMetric float64,
+// 	severity float64,
+// 	testsAffected int,
+// 	goodPeriodInfo string,
+// 	badPeriodInfo string,
+// 	incidentInfo string) {
 
-	i.GoodPeriodStart = goodTimeStart
-	i.GoodPeriodEnd = goodTimeEnd
-	i.BadPeriodStart = badTimeStart
-	i.GoodPeriodMetric = goodMetric
-	i.BadPeriodMetric = badMetric
-	i.BadPeriodEnd = badTimeEnd
-	i.Severity = severity
-	i.NumTestsAffected = testsAffected
-	i.GoodPeriodInfo = goodPeriodInfo
-	i.BadPeriodInfo = badPeriodInfo
-	i.IncidentInfo = incidentInfo
-}
+// 	i.GoodPeriodStart = goodTimeStart
+// 	i.GoodPeriodEnd = goodTimeEnd
+// 	i.BadPeriodStart = badTimeStart
+// 	i.GoodPeriodMetric = goodMetric
+// 	i.BadPeriodMetric = badMetric
+// 	i.BadPeriodEnd = badTimeEnd
+// 	i.Severity = severity
+// 	i.NumTestsAffected = testsAffected
+// 	i.GoodPeriodInfo = goodPeriodInfo
+// 	i.BadPeriodInfo = badPeriodInfo
+// 	i.IncidentInfo = incidentInfo
+// }
 
-func (i *DefaultIncident) Init(goodTimeStart time.Time, goodTimeEnd time.Time,
-	badTimeStart time.Time,
-	badTimeEnd time.Time,
-	avgDSGood float64,
-	avgDSBad float64,
-	severity float64,
-	testsAffected int) {
+// func (i *DefaultIncident) Init(goodTimeStart time.Time, goodTimeEnd time.Time,
+// 	badTimeStart time.Time,
+// 	badTimeEnd time.Time,
+// 	avgDSGood float64,
+// 	avgDSBad float64,
+// 	severity float64,
+// 	testsAffected int) {
 
-	i.goodStartTime = goodTimeStart
-	i.goodEndTime = goodTimeEnd
-	i.badStartTime = badTimeStart
-	i.badEndTime = badTimeEnd
-	i.avgGoodDS = avgDSGood
-	i.avgBadDS = avgDSBad
-	i.severityDecimal = severity
-	i.numTestsAffected = testsAffected
-}
+// 	i.goodStartTime = goodTimeStart
+// 	i.goodEndTime = goodTimeEnd
+// 	i.badStartTime = badTimeStart
+// 	i.badEndTime = badTimeEnd
+// 	i.avgGoodDS = avgDSGood
+// 	i.avgBadDS = avgDSBad
+// 	i.severityDecimal = severity
+// 	i.numTestsAffected = testsAffected
+// }
 
 //**************************************//
 //    		GETTER METHODS
 //**************************************//
 
-func (i *DefaultIncident) GetGoodPeriod() (time.Time, time.Time) {
-	return i.goodStartTime, i.goodEndTime
-}
+// func (i *DefaultIncident) GetGoodPeriod() (time.Time, time.Time) {
+// 	return i.goodStartTime, i.goodEndTime
+// }
 
-func (i *DefaultIncident) GetBadPeriod() (time.Time, time.Time) {
-	return i.badStartTime, i.badEndTime
-}
+// func (i *DefaultIncident) GetBadPeriod() (time.Time, time.Time) {
+// 	return i.badStartTime, i.badEndTime
+// }
 
-func (i *DefaultIncident) GetGoodMetric() float64 {
-	return i.avgGoodDS
-}
+// func (i *DefaultIncident) GetGoodMetric() float64 {
+// 	return i.avgGoodDS
+// }
 
-func (i *DefaultIncident) GetBadMetric() float64 {
-	return i.avgBadDS
-}
+// func (i *DefaultIncident) GetBadMetric() float64 {
+// 	return i.avgBadDS
+// }
 
-func (i *DefaultIncident) GetSeverity() float64 {
-	return i.severityDecimal
-}
+// func (i *DefaultIncident) GetSeverity() float64 {
+// 	return i.severityDecimal
+// }
 
-func (i *DefaultIncident) GetTestsAffected() int {
-	return i.numTestsAffected
-}
+// func (i *DefaultIncident) GetTestsAffected() int {
+// 	return i.numTestsAffected
+// }
 
-func (i *DefaultIncident) GetGoodPeriodInfo() string {
-	ds := strconv.FormatFloat(i.avgGoodDS, 'f', 2, 64)
-	return "Average download speed: " + ds + " mb/s"
-}
+// func (i *DefaultIncident) GetGoodPeriodInfo() string {
+// 	ds := strconv.FormatFloat(i.avgGoodDS, 'f', 2, 64)
+// 	return "Average download speed: " + ds + " mb/s"
+// }
 
-func (i *DefaultIncident) GetBadPeriodInfo() string {
-	ds := strconv.FormatFloat(i.avgBadDS, 'f', 2, 64)
-	return "Average download speed: " + ds + " mb/s"
-}
+// func (i *DefaultIncident) GetBadPeriodInfo() string {
+// 	ds := strconv.FormatFloat(i.avgBadDS, 'f', 2, 64)
+// 	return "Average download speed: " + ds + " mb/s"
+// }
 
-func (i *DefaultIncident) GetIncidentInfo() string {
-	s := strconv.FormatFloat(i.severityDecimal*100, 'f', 2, 64)
-	ta := strconv.Itoa(i.numTestsAffected)
-	return "Download speed dropped by " + s + "% affecting " + ta + " tests"
-}
+// func (i *DefaultIncident) GetIncidentInfo() string {
+// 	s := strconv.FormatFloat(i.severityDecimal*100, 'f', 2, 64)
+// 	ta := strconv.Itoa(i.numTestsAffected)
+// 	return "Download speed dropped by " + s + "% affecting " + ta + " tests"
+// }
