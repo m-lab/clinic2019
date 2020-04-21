1ccf8e4e9fd37fe0210870a88329194296afb16f
Jacqui Giese <jacquigiese@Jacquis-MacBook-Pro.local>
2020-04-21 14:32:32 -0700

    store changes before merge

--- pipeline/pipeline.go
+++ pipeline/pipeline.go
@@ -1,6 +1,7 @@
 package pipeline
 
 import (
+	"os"
 	"os/exec"
 
 	//csvParser "github.com/m-lab/clinic2019/CsvParser"
@@ -14,6 +15,7 @@ type IncidentProducer interface {
 type AnalyzerIncidents struct{}
 
 func (ai *AnalyzerIncidents) findIncidents() {
+	// I think this will generate the CSV without needing a cmd.Run()
 	exec.Command("bash", "-c", "go run github.com/m-lab/signal-searcher  | sort -nk1 > incidents.csv").Output()
 }
 
@@ -21,7 +23,10 @@ func runPipeline() []incident.DefaultIncident {
 	// Run script that generates CSV
 	var i AnalyzerIncidents
 	i.findIncidents()
-	csvParser.createHierarchy("incidents.csv", "/Users/jacquigiese/Desktop/incidents")
+	// Use os.Getenv("HOME") to get the /Users/[username] path
+	// When submitting, replace bucket name with "incidents-location-hierarchy"
+	csvParser.createHierarchy("incidents.csv", "/Users/jacquigiese/Desktop/incidentFileHierarchy", "incident_mounting_test")
 	// TODO: remove the csv file
+	os.Remove("incidents.csv")
 
 }
